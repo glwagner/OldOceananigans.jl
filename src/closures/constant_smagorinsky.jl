0a1c86129a78b6d33d89e69c9cc597811e7a3d29
Gregory Wagner <wagner.greg@gmail.com>
2019-05-29 11:30:53 -0400

    Const-ifies the filter width function aliases

--- src/closures/constant_smagorinsky.jl
+++ src/closures/constant_smagorinsky.jl
@@ -12,16 +12,17 @@ end
 
 Returns a `ConstantSmagorinsky` closure object of type `T` with
 
-* `C`            : Smagorinsky constant
-* `Pr`           : Prandtl number
-* `ν_background` : background viscosity
-* `κ_background` : background diffusivity
+    * `C`            : Smagorinsky constant
+    * `Pr`           : Prandtl number
+    * `ν_background` : background viscosity
+    * `κ_background` : background diffusivity
+
 """
 ConstantSmagorinsky(T; kwargs...) =
       typed_keyword_constructor(T, ConstantSmagorinsky; kwargs...)
 
 "Return the filter width for Constant Smagorinsky on a Regular Cartesian grid."
-@inline Δ(i, j, k, grid::RegularCartesianGrid, ::ConstantSmagorinsky) = geo_mean_Δ(grid)
+@inline Δ(i, j, k, grid::RegularCartesianGrid{T}, ::ConstantSmagorinsky{T}) where T = geo_mean_Δ(grid)
 
 # tr_Σ² : ccc
 #   Σ₁₂ : ffc
@@ -69,10 +70,10 @@ end
 end
 
 # Temporarily set filter widths to cell-size (rather than distance between cell centers, etc.)
-Δ_ccc = Δ
-Δ_ffc = Δ
-Δ_fcf = Δ
-Δ_cff = Δ
+const Δ_ccc = Δ
+const Δ_ffc = Δ
+const Δ_fcf = Δ
+const Δ_cff = Δ
 
 """
     buoyancy(i, j, k, grid, eos, g, T, S)
@@ -80,8 +81,8 @@ end
 Calculate the buoyancy at grid point `i, j, k` associated with `eos`, 
 gravitational acceleration `g`, temperature `T`,  and salinity `S`.
 """
-@inline buoyancy(i, j, k, grid, eos::LinearEquationOfState, g, T, S) = 
-    g * ( eos.βT * (T[i, j, k] - eos.T₀) - eos.βS * (S[i, j, k] - eos.S₀) )
+@inline buoyancy(i, j, k, grid, eos::LinearEquationOfState, grav, T, S) = 
+    grav * ( eos.βT * (T[i, j, k] - eos.T₀) - eos.βS * (S[i, j, k] - eos.S₀) )
 
 """
     stability(N², Σ², Pr, Cb)
@@ -92,7 +93,7 @@ Return the stability function
 
 when ``N^2 > 0``, and 1 otherwise.
 """
-@inline stability(N², Σ², Pr, Cb::T) where T = min(one(T), max(zero(T), sqrt(one(T) - Cb * N² / (Pr*Σ²))))
+@inline stability(N²::T, Σ²::T, Pr::T, Cb::T) where T = min(one(T), max(zero(T), sqrt(one(T) - Cb * N² / (Pr*Σ²))))
 
 """
     νₑ(ς, Cs, Δ, Σ²)
