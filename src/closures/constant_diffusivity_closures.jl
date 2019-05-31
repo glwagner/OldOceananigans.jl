6fba4ff10fef417ac162930f023808f0da6d3170
Gregory Wagner <wagner.greg@gmail.com>
2019-05-31 08:54:54 -0400

    pastes old (fast) operators into constant isotropic diffusivity flux divergenc operators

--- src/closures/constant_diffusivity_closures.jl
+++ src/closures/constant_diffusivity_closures.jl
@@ -30,6 +30,31 @@ ConstantIsotropicDiffusivity(T; kwargs...) =
 @inline ν_cff(i, j, k, grid, closure::ConstantIsotropicDiffusivity, args...) = closure.ν
 
 @inline ∂ⱼ_2ν_Σ₁ⱼ(i, j, k, grid, closure::ConstantIsotropicDiffusivity, eos, g, u, v, w, T, S) = (
+      closure.ν/grid.Δx^2 * δx²_f2c2f(grid, u, i, j, k) 
+    + closure.ν/grid.Δy^2 * δy²_f2e2f(grid, u, i, j, k)
+    + closure.ν/grid.Δz^2 * δz²_f2e2f(grid, u, i, j, k)
+)
+
+@inline ∂ⱼ_2ν_Σ₂ⱼ(i, j, k, grid, closure::ConstantIsotropicDiffusivity, eos, g, u, v, w, T, S) = (
+      closure.ν/grid.Δx^2 * δx²_f2e2f(grid, v, i, j, k) 
+    + closure.ν/grid.Δy^2 * δy²_f2c2f(grid, v, i, j, k)
+    + closure.ν/grid.Δz^2 * δz²_f2e2f(grid, v, i, j, k)
+)
+
+@inline ∂ⱼ_2ν_Σ₃ⱼ(i, j, k, grid, closure::ConstantIsotropicDiffusivity, eos, g, u, v, w, T, S) = (
+      closure.ν/grid.Δx^2 * δx²_f2e2f(grid, w, i, j, k) 
+    + closure.ν/grid.Δy^2 * δy²_f2e2f(grid, w, i, j, k)
+    + closure.ν/grid.Δz^2 * δz²_f2c2f(grid, w, i, j, k)
+)
+
+@inline ∇_κ_∇ϕ(i, j, k, grid, ϕ, closure::ConstantIsotropicDiffusivity, args...) = (
+      closure.κ/grid.Δx^2 * δx²_c2f2c(grid, ϕ, i, j, k)
+    + closure.κ/grid.Δy^2 * δy²_c2f2c(grid, ϕ, i, j, k)
+    + closure.κ/grid.Δz^2 * δz²_c2f2c(grid, ϕ, i, j, k)
+)
+
+#=
+@inline ∂ⱼ_2ν_Σ₁ⱼ(i, j, k, grid, closure::ConstantIsotropicDiffusivity, eos, g, u, v, w, T, S) = (
       closure.ν * ∂x²_faa(i, j, k, grid, u)
     + closure.ν * ∂y²_aca(i, j, k, grid, u)
     + closure.ν * ∂z²_aac(i, j, k, grid, u)
@@ -46,6 +71,7 @@ ConstantIsotropicDiffusivity(T; kwargs...) =
     + closure.ν * ∂y²_aca(i, j, k, grid, w)
     + closure.ν * ∂z²_aaf(i, j, k, grid, w)
     )
+=#
 
 #
 # Constant anisotropic diffusivity (tensor diffusivity with heterogeneous
